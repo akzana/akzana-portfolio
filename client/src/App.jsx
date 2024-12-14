9f2bc48cad1bf7bb40ddde2c0eb8c694911849ac
akzana <akzanaklepsch@outlook.com>
2024-12-13 20:06:37 -0800

    🔨 add imports & @use

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Header from "./components/Header/Header.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import AboutPage from './pages/AboutPage/AboutPage.jsx';
 
@@ -8,6 +8,7 @@ import AboutPage from './pages/AboutPage/AboutPage.jsx';
 export default function App() {
   return (
     <Router>
+    <Header/>
       <Routes>
         <Route link="/" element={HomePage} />
         <Route link="/about" element={AboutPage} />
