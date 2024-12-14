5043fd231557591c0cca91834406aa0d6305df40
akzana <akzanaklepsch@outlook.com>
2024-12-13 21:34:23 -0800

    🔨 add Nav component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Header.scss";
+import Nav from '../Nav/Nav';
 
 export default function Header() {
   return (
     <div>
-      
+      <Nav/>
     </div>
   )
 }
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -4,7 +4,13 @@ import "./Nav.scss";
 export default function Nav() {
   return (
     <div>
-      
+      <nav className="nav">
+        <ul className="nav__ul">
+          <li className="nav__li">Home</li>
+          <li className="nav__li">About</li>
+          <li className="nav__li">Resume</li>
+        </ul>
+      </nav>
     </div>
   )
 }
