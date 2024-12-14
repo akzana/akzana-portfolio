634cc626dceedf42d0743568750725823d2f153b
akzana <akzanaklepsch@outlook.com>
2024-12-13 19:54:58 -0800

    🔨 add components dev scripts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import AboutPage from './pages/AboutPage/AboutPage.jsx';
 
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route link="/" element={HomePage} />
+        <Route link="/about" element={AboutPage} />
+      </Routes>
+    </Router>
   )
 }
