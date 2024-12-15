41a862acbe8ab7c4a74ab30607c0e4e1c75bd003
akzana <akzanaklepsch@outlook.com>
2024-12-14 16:08:09 -0800

    🔥 🔨 🚧 🔧 add template folder; add header/footer components; remove redundant

---
+++ .fttemplates/FTName/FTName.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "FTName.scss";
+
+export default function FTName() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Header.scss";
-import Nav from '../Nav/Nav';
+import Nav from './Nav/Nav.jsx';
+import Logo from './Logo/Logo.jsx';
 
 export default function Header() {
   return (
     <div>
+      <Logo/>
       <Nav/>
     </div>
   )
